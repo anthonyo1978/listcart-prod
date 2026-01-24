79fd96e14b05e69f69c7916a6e56e5bae4b87d78
Anthony O'Reilly <anthonyo1978@gmail.com>
2026-01-24 18:53:18 +1030

    Fix: status -> itemStatus in carts API

--- app/api/carts/route.ts
+++ app/api/carts/route.ts
@@ -26,7 +26,7 @@ export async function GET() {
           select: {
             id: true,
             selected: true,
-            status: true,
+            itemStatus: true,
           },
         },
       },
